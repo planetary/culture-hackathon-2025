60651306b3b9d3a795816afc228bfc6018673e32
ChrisLaRocque <larocque.christopher@gmail.com>
2025-04-16 12:49:56 -0400

    Update sanity.cli.ts

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'aommjgk1',
-    dataset: 'production'
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
